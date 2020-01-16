2fe1da532c1dacded9efc9676619ca7e94da6d50
Clément Muth <clement.muth@epitech.eu>
2020-01-16 18:53:01 +0100

    [SAVE]

--- sources/process/process_ftc2.c
+++ sources/process/process_ftc2.c
@@ -7,22 +7,33 @@
 
 #include "main/main.h"
 
-static char **my_setenv(main_t *shell, char **env)
+void setenv_fct(main_t *shell)
 {
     size_t size = 0;
 
-    for (; env[size]; ++size);
-    env[size] = my_strdup(my_strcat(my_strcat(av[1], "="), av[2]));
-    env[size + 1] = NULL;
-}
-
-void setenv_fct(main_t *shell)
-{
-    for (int i = 0; shell->evrnmt[i]; ++i);
-    my_setenv(shell, shell->evrnmt);
+    if (!av[1] || !av[2] || ac != 3) {
+        my_putstrc("setenv\nminishell: setenv: variable and value necessary\n",
+        RED, true);
+        return;
+    }
+    for (; shell->evrnmt[size]; ++size);
+    shell->evrnmt[size] = my_strdup(my_strcat(my_strcat(av[1], "="), av[2]));
+    shell->evrnmt[size + 1] = NULL;
 }
 
 void unsetenv_fct(main_t *shell)
 {
-
+    if (!av[1] || ac != 2) {
+        my_putstrc("unsetenv\nminishell: unsetenv: variable name necessary\n",
+        RED, true);
+        return;
+    }
+    for (int i = 0; shell->evrnmt[i]; ++i)
+        if (my_strcmp(av[1], shell->evrnmt[i]) == true) {
+            my_rm_line_array(shell->evrnmt, i);
+            my_putstrc("variable remove successfully\n", GREEN, true);
+            return;
+        }
+    my_putstrc("unsetenv\nminishell: unsetenv: variable name not found\n",
+    RED, true);
 }
